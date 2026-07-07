55b1cf2d8e4db57f3fa8ccc3ecc322cd4373fc8c
lemonhuhh <tamangprabin711@gmail.com>
2026-07-07 13:47:07 +0545

    Learned about conditions

--- js/script.js
+++ js/script.js
@@ -1,40 +1,130 @@
-console.log("============================================")
-console.log("Hello form external script.")
-console.log("============================================")
-console.log("List of 10 foods:")
-console.log("1.Cake")
-console.log("2.Burger")
-console.log("3.Pizza")
-console.log("4.MO:MO")
-console.log("5.Samosa")
-console.log("6.Chicken Chilly")
-console.log("7.Fried Rice")
-console.log("8.Dal Bhat")
-console.log("9.Biryani")
-console.log("10.Sekwa")
+// console.log("============================================")
+// console.log("Hello form external script.")
+// console.log("============================================")
+// console.log("List of 10 foods:")
+// console.log("1.Cake")
+// console.log("2.Burger")
+// console.log("3.Pizza")
+// console.log("4.MO:MO")
+// console.log("5.Samosa")
+// console.log("6.Chicken Chilly")
+// console.log("7.Fried Rice")
+// console.log("8.Dal Bhat")
+// console.log("9.Biryani")
+// console.log("10.Sekwa")
 
-alert("Hello welcome to my website")
-// alert("Are you sure you want to quit!")
-// alert("Do you want to delete it permanently?")
-// alert("Your message successfully sent")
-// alert("Are you sure you want to proceed?")
-// alert("You are currently on the free plan.")
-// alert("Your email address is invalid")
-// alert("Something went worng")
+// alert("Hello welcome to my website")
+// // alert("Are you sure you want to quit!")
+// // alert("Do you want to delete it permanently?")
+// // alert("Your message successfully sent")
+// // alert("Are you sure you want to proceed?")
+// // alert("You are currently on the free plan.")
+// // alert("Your email address is invalid")
+// // alert("Something went worng")
 
 
-// console.log(prompt("Enter your username:"))
-// console.log(prompt("Enter your email:"))
-// console.log(prompt("Enter your password:"))
-// console.log(prompt("Enter your age:"))
-// console.log(prompt("Enter your address:"))
-// console.log(prompt("Enter your message:"))
-// console.log(prompt("Enter your Father's Name"))
-// console.log(prompt("Enter your Mother's Name"))
+// // console.log(prompt("Enter your username:"))
+// // console.log(prompt("Enter your email:"))
+// // console.log(prompt("Enter your password:"))
+// // console.log(prompt("Enter your age:"))
+// // console.log(prompt("Enter your address:"))
+// // console.log(prompt("Enter your message:"))
+// // console.log(prompt("Enter your Father's Name"))
+// // console.log(prompt("Enter your Mother's Name"))
 
-confirm("Are you sure you want to submit?")
-confirm("Are you sure you want to buy this item?")
-confirm("Are you sure you want to quit?")
-confirm("Are you sure you want to cancel the payment?")
-confirm("Are you sure you want to place this order?")
-confirm("Are you sure you want to copy this page?")
+// confirm("Are you sure you want to submit?")
+// confirm("Are you sure you want to buy this item?")
+// confirm("Are you sure you want to quit?")
+// confirm("Are you sure you want to cancel the payment?")
+// confirm("Are you sure you want to place this order?")
+// confirm("Are you sure you want to copy this page?")
+
+// let name = "John"
+// console.log("Name:", name)
+
+// const gender ="Male"
+// console.log("Gender:" ,gender)
+
+// var emotion = "Happy"
+// console.log("Emotion:" ,emotion)
+
+// console.log("=====================================================")
+
+// name = "Kelvin"
+// console.log("Name:", name)
+// console.log("Gender:", gender)
+// emotion = "Sad"
+// console.log("Emotion:" ,emotion)
+
+// console.log("=====================================================")
+
+
+// // Add two numbers
+
+// // let a = 1098, b = 2039
+// // console.log("a+b=", a+b)
+
+// //Celsius to fahrenheit
+
+// let celsius = 30
+// console.log("Fahrenheit=", (celsius * 9/5) +32)
+
+// console.log("=====================================================")
+
+// //CONDITIONS
+
+
+// //ODD OR EVEN
+// let a = 27
+// if(a%2==0){
+//     console.log(a , "is even")
+// }
+// else
+// {
+//     console.log(a, "is odd")
+// }
+
+// console.log("=====================================================")
+
+//eligible or not
+
+let age = 12
+console.log("Age:", age)
+if(age>=18){
+    console.log("You are eligible.")
+}
+else{
+    console.log("Your are not eligible for voting.")
+}
+
+console.log("=====================================================")
+
+//Largest among two numbers
+
+let e = 348, f = 233
+console.log("Numbers are:", e  ,"and", f)
+if(e>f)
+{
+    console.log(e, "is the largest.")
+}
+else{
+    console.log(f, "is the largest.")
+}
+
+console.log("=====================================================")
+
+//Assign grade
+
+let grade = Number(prompt("Enter your grade:"))
+console.log("Grade is:", grade)
+if(grade>=3.2 && grade<=3.6)
+{
+    console.log("Your grade is A")
+}
+else if(grade>=3.6 && grade<=4)
+{
+    console.log("Your grade is A+")
+}
+else{
+    console.log("Fail")
+}
