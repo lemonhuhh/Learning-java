3ffbbf0c4192ad4275599bb75f4a4d48ff58f161
lemonhuhh <tamangprabin711@gmail.com>
2026-07-08 13:55:00 +0545

    Learned variables

--- js/script.js
+++ js/script.js
@@ -88,43 +88,146 @@
 
 //eligible or not
 
-let age = 12
-console.log("Age:", age)
-if(age>=18){
-    console.log("You are eligible.")
-}
-else{
-    console.log("Your are not eligible for voting.")
-}
+// let age = Number(prompt("Enter your age:"))
+// console.log("Age:", age)
+// if(age>=18){
+//     console.log("You are eligible.")
+// }
+// else{
+//     console.log("Your are not eligible for voting.")
+// }
 
-console.log("=====================================================")
+// console.log("=====================================================")
 
-//Largest among two numbers
+// //Largest among two numbers
 
-let e = 348, f = 233
-console.log("Numbers are:", e  ,"and", f)
-if(e>f)
-{
-    console.log(e, "is the largest.")
-}
-else{
-    console.log(f, "is the largest.")
-}
+// let e = 348, f = 233
+// console.log("Numbers are:", e  ,"and", f)
+// if(e>f)
+// {
+//     console.log(e, "is the largest.")
+// }
+// else{
+//     console.log(f, "is the largest.")
+// }
 
-console.log("=====================================================")
+// console.log("=====================================================")
 
-//Assign grade
+// //Assign grade
 
-let grade = Number(prompt("Enter your grade:"))
-console.log("Grade is:", grade)
-if(grade>=3.2 && grade<=3.6)
-{
-    console.log("Your grade is A")
-}
-else if(grade>=3.6 && grade<=4)
-{
-    console.log("Your grade is A+")
-}
-else{
-    console.log("Fail")
-}
+// let grade = Number(prompt("Enter your grade:"))
+// console.log("Grade is:", grade)
+// if(grade>=3.2 && grade<=3.6)
+// {
+//     console.log("Your grade is A")
+// }
+// else if(grade>=3.6 && grade<=4)
+// {
+//     console.log("Your grade is A+")
+// }
+// else{
+//     console.log("Fail")
+// }
+
+// //LOOPS
+
+// //Print number from 1 to 1000
+
+// for(i=1; i<=1000; i++)
+// {
+//     console.log("From:", i)
+// }
+
+// console.log("=====================================================")
+
+// var myfavmovie = "Obsession"
+// console.log("List of my favorite movies:")
+// console.log("1.", myfavmovie)
+
+// myfavmovie = "Interstellar"
+// console.log("2.", myfavmovie)
+
+// var myfavsong = "Style"
+// console.log("My favorite song:", myfavsong)
+
+
+
+
+
+// let myfavmovie = "Obsession"
+// console.log("List of my favorite movies:")
+// console.log("1.", myfavmovie)
+
+// myfavmovie = "Intersteller"
+// console.log("2.", myfavmovie)
+
+// let myfavsong = "Style"
+// console.log("List of my favorite song:")
+// console.log("1.", myfavmovie)
+
+// myfavsong = "You belong with me"
+// console.log("2.", myfavsong)
+
+// let myfavfood = "Pizza"
+// console.log("List of my favorite food:")
+// console.log("1.", myfavfood)
+
+// myfavfood = "Burger"
+// console.log("2.", myfavfood)
+
+
+
+// const myfavmovie = "Obsession"
+// console.log("My favorite movie:", myfavmovie)
+
+// const myfavsong = "Style"
+// console.log("My favorite song:", myfavmovie)
+
+// const myfavfood = "Pizza"
+// console.log("My favorite food:", myfavfood)
+
+
+
+
+// let myfavperson = "Jimmy"
+// console.log(myfavperson)
+
+// myfavperson = "Jimmy Donaldson, known as MrBeast, is the world's biggest YouTuber, famous for turning massive viral stunts into high-impact philanthropy like gifting houses, funding surgeries, and planting trees."
+// console.log(myfavperson)
+
+// var myfavplace = "Pokhara"
+// console.log(myfavplace)
+
+// myfavplace = "Pokhara is Nepal's ultimate adventure and tourism hub, famous for its stunning, mirror-like views of the Annapurna range reflecting off Phewa lake. It serves as the gateway for world-class trekking routine."
+// console.log(myfavplace)
+
+
+// const pi = parseFloat(prompt("Enter the value:"))
+// console.log("Value is:", pi)
+
+
+
+
+// let num = parseInt(prompt("Enter your favorite number"))
+// console.log("The number is:", num)
+
+// var deci = parseFloat(prompt("Enter the number:"))
+// console.log("Number is:", deci)
+
+
+
+// const isAvailable = true
+// const isNotavailable = false
+
+// const instock = prompt("Is the item in stock?" (true/false))
+// const outofstock = prompt("Is the item in stock?" (true/false))
+// console.log("Is the item in stock", instock)
+// console.log("Is the item in stock", outofstock)
+
+
+
+let a = null
+console.log("The value is:", a)
+
+let b
+console.log("The value is:", b)
