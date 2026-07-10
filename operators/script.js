53fcd93110fb57922ab84c044ead678a533845d6
lemonhuhh <tamangprabin711@gmail.com>
2026-07-10 12:38:13 +0545

    Learned arithmetic operators

---
+++ operators/script.js
@@ -0,0 +1,39 @@
+console.log("============================================")
+
+// let a = Number(prompt("Enter your first number:"))
+// let b = Number(prompt("Enter your second number:"))
+// let c = Number(prompt("Enter your third number"))
+// let sum = a+b+c
+// console.log(`The sum of your numbers ${a} , ${b} and ${c} is: ${sum}`)
+
+// console.log("============================================")
+
+
+// let a = Number(prompt("Enter your first number:"))
+// let b = Number(prompt("Enter your second number:"))
+// let c = Number(prompt("Enter your third number"))
+// let mul = a*b*c
+// console.log(`The multiplication of your numbers ${a} , ${b} and ${c} is: ${mul}`)
+
+// console.log("============================================")
+
+
+// let a = Number(prompt("Enter your first number:"))
+// let b = Number(prompt("Enter your second number:"))
+// let c = Number(prompt("Enter your third number"))
+// let sub = a-b
+// let subt = sub-c
+// console.log(`The Subtraction of your numbers ${a} , ${b} and ${c} is: ${subt}`)
+
+// console.log("============================================")
+
+// let a = Number(prompt("Enter your first number:"))
+// let b = Number(prompt("Enter your second number:"))
+// let c = Number(prompt("Enter your third number"))
+// let div = a/b/c
+// console.log(`The Divison of your numbers ${a} , ${b} and ${c} is: ${div}`)
+
+// console.log("============================================")
+
+// let a = Number(prompt("Enter your first number:"))
+// console.log(`The increement is: ${++a}`)
